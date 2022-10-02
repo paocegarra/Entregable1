bbf7879ce994f55cc8082d93e48eac8f2a801daf
paocegarra <paocegarra2@gmail.com>
2022-10-02 14:33:09 -0300

    agrego la primer vista

--- ProyectoCoder/ProyectoCoder/urls.py
+++ ProyectoCoder/ProyectoCoder/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AppCoder.views import mostrar_inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Inicio/', mostrar_inicio),
 ]
